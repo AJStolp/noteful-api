c707ab3c32f8d51147bdb22324c2f47bdc7eeeea
Anthony Stolp <Anthony.Stolp@gmail.com>
2020-05-01 14:02:07 -0500

    tables created and migrated into database

---
+++ seeds/seed.noteful_notes.sql
@@ -0,0 +1,8 @@
+INSERT INTO Noteful_notes (note_title, note_content)
+VALUES
+    ('Title 1', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.'),
+    ('Title 2', 'Nam pellentesque nisi nec blandit condimentum.'),
+    ('Title 3', 'Praesent eu erat rhoncus, hendrerit purus eget, sodales nibh.'),
+    ('Title 4', 'Sed condimentum tellus nec magna convallis, id pulvinar risus consequat.'),
+    ('Title 5', 'In a dui vel dui tempus imperdiet.'),
+    ('Title 6', 'In dapibus turpis sit amet neque scelerisque commodo.');
